85685041ae6c84a037cb0be045632e5e1391499e
thadakalavasu19 <kumarvasu602@gmail.com>
2025-05-28 08:38:41 +0530

    Create header.js

---
+++ header.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Header = () => (
+  <header className="bg-primary text-white p-3 text-center">
+    <h1>My E-Commerce Store</h1>
+  </header>
+);
+
+export default Header;
