dee65a1a1ea85b97465546fd0a26967cfa4ff173
thadakalavasu19 <kumarvasu602@gmail.com>
2025-05-28 08:39:09 +0530

    Create navbar.js

---
+++ navbar.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => (
+  <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div className="container-fluid">
+      <Link className="navbar-brand" to="/">E-Shop</Link>
+      <div className="collapse navbar-collapse">
+        <ul className="navbar-nav">
+          <li className="nav-item"><Link className="nav-link" to="/">Home</Link></li>
+          <li className="nav-item"><Link className="nav-link" to="/about">About</Link></li>
+          <li className="nav-item"><Link className="nav-link" to="/register">Product Registration</Link></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+);
+
+export default Navbar;
